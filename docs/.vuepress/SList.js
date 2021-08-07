9f2fd0bc73c8c314765c6c0a00fac051e9cd3287
Hitesh Kumar <hitesh.k.83080@gmail.com>
2021-08-07 12:08:09 +0530

    Update SList.js

--- docs/.vuepress/SList.js
+++ docs/.vuepress/SList.js
@@ -13,7 +13,9 @@ exports.PyList = [
 
 exports.DSList = [
     'Data-Structures/',
-    'Data-Structures/SinglyLinkedList.md'
+    'Data-Structures/SinglyLinkedList.md','Data-Structures/AVL_Tree.md','Data-Structures/BST.md','Data-Structures/BinaryTree.md','Data-Structures/Circular_DLList.md',
+    'Data-Structures/Circular_SLList.md','Data-Structures/Doubly_LinkedList.md','Data-Structures/Heap.md','Data-Structures/Queue.md','Data-Structures/Queue_using _List.md',
+    'Data-Structures/Stack.md','Data-Structures/Stack_using_Llist.md','Data-Structures/Stack_using_list.md','Data-Structures/Trie.md','Data-Structures/tree.md',
 ]
 
 exports.AlgoList = [
